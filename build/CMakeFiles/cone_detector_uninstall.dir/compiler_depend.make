5bbd925ceb770bc3006ddf6a8f16b328f767a2f7
alebraglia <alebraglia1@hotmail.com>
2025-04-02 19:42:28 +0200

    removed build and .vscode dir

--- build/CMakeFiles/cone_detector_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for cone_detector_uninstall.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alebraglia/Cuda_clustering/cone_detector
-# Build directory: /home/alebraglia/Cuda_clustering/cone_detector/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
